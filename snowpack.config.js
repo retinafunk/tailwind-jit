de1f45256b167190516a4d91aa0ca45f86778d54
retinafunk <retinafunk@gmail.com>
2021-07-09 16:23:28 +0200

    Config dev tools :  snowpack & tailwind

---
+++ snowpack.config.js
@@ -0,0 +1,21 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    '@snowpack/plugin-postcss',
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    tailwindConfig: './tailwind.config.js'
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
